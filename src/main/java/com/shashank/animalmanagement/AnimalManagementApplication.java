677e980aa9d25d8bb3cedfdeff4f90e66b790610
shashank sanket <shashank.sanket@SW-LP09407.local>
2020-05-17 01:48:35 +0530

    added file for lession26

---
+++ src/main/java/com/shashank/animalmanagement/AnimalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.shashank.animalmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalManagementApplication.class, args);
+	}
+
+}
